7c7a0c8fecb1f2ccf08f9fbef7c6bc535b8152de
Glitch (hello-webpage) <none>
2023-12-11 12:38:54 +0000

    ☂️🎡 Checkpoint ./css/my-first-stylesheet.css:63066070/54 ./see-us-play.html:63066070/82:63067065/109 ./script.js:63066070/2:63067065/177

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function hello() {
   document.getElementById("x").innerHTML =
     "PLAY THE GAME AND BE A GOOD PERSON!!! ";
 }
+function hi() {
+  document.getElementById("bigbutton").innerHTML =
+    "ITS RIGHT BELOW THIS BUTTON YOU SMART PERSON ";
+}
