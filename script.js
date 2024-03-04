4077d4e108800a8c56e8a44bfb0b0bf90e637143
Glitch (hello-webpage) <none>
2024-03-04 12:39:16 +0000

    🌖🌪 Checkpoint ./futureprojects.html:63066266/35 ./about-us.html:63066070/4364:63066266/572 ./mainpage.html:63066070/10132:63066266/496 ./css/my-first-stylesheet.css:63066070/43 ./game.html:63066070/893 ./gamecode.html:63066070/2236 ./index.html:63066070/1420 ./script.js:63066070/1588

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ let confirmbutton = false;
 function hello() {
   if (state === false) {
     state = true;
-    document.getElementById("x").innerHTML = "PLAY THE GAME AND BE A GOOD PERSON!!!";
+    document.getElementById("x").innerHTML =
+      "PLAY THE GAME AND BE A GOOD PERSON!!!";
     console.log(state);
   } else {
     state = false;
-    document.getElementById("x").innerHTML = "Retros: Tower of Doom"; 
+    document.getElementById("x").innerHTML = "Retros: Tower of Doom";
     console.log(state);
   }
 }
 
 function hi() {
-  document.getElementById("bigbutton").innerHTML = "ITS RIGHT BELOW THIS BUTTON YOU SMART PERSON";
+  document.getElementById("bigbutton").innerHTML =
+    "ITS RIGHT BELOW THIS BUTTON YOU SMART PERSON";
 }
 function agelessthanthirteen() {
   confirm = true;
@@ -22,11 +24,10 @@ function agelessthanthirteen() {
 }
 function confirmb() {
   let confirmbutton = true;
-  let text = document.getElementById("text").value
+  let text = document.getElementById("text").value;
   console.log(confirmbutton);
   if (text === "13+") {
     location.replace("https://towerofdoom.glitch.me/mainpage.html");
   } else {
-    
   }
-};
+}
